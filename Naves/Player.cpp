acdb461dfaab33256715d4e04ecb4e8702838ca4
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-09-20 13:27:23 +0200

    Implemented player limit bounding

--- Naves/Player.cpp
+++ Naves/Player.cpp
@@ -12,7 +12,19 @@ void Player::update() {
 		shootTime--;
 	}
 	x = x + vx;
+	if (x - width/2 < 0) {
+		x = width/2;
+	}
+	else if (x + width/2 > WIDTH) {
+		x = WIDTH - width / 2;
+	}
 	y = y + vy;
+	if (y - height/2 < 0) {
+		y = height/2;
+	}
+	else if (y + height/2 > HEIGHT) {
+		y = HEIGHT - height / 2;
+	}
 }
 
 void Player::moveX(float direction) {
